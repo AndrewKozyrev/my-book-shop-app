fc7ed7a43d565404c80ce38750fb7842657b52d5
AndrewKozyrev <b2e3t8a2>
2022-04-03 18:45:27 +0300

    - подключил страницу “Жанры” (genres/index.html) - подключил страницу “Авторы” (authors/index.html) - дополнил схему базы данных таблицей authors

--- src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/MainPageController.java
@@ -1,5 +1,6 @@
 package com.example.MyBookShopApp.controllers;
 
+import com.example.MyBookShopApp.data.AuthorService;
 import com.example.MyBookShopApp.data.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
--- src/main/java/com/example/MyBookShopApp/data/BookService.java
+++ src/main/java/com/example/MyBookShopApp/data/BookService.java
@@ -23,10 +23,18 @@ public class BookService {
         List<Book> books = jdbcTemplate.query("SELECT * FROM books", (ResultSet rs, int rownum)->{
             Book book = new Book();
             book.setId(rs.getInt("id"));
-            book.setAuthor(rs.getString("author"));
             book.setTitle(rs.getString("title"));
-            book.setPriceOld(rs.getString("priceOld"));
+            book.setPriceOld(rs.getString("price_old"));
             book.setPrice(rs.getString("price"));
+            int authorId =rs.getInt("author_id");
+            Author author = jdbcTemplate.queryForObject("SELECT * FROM authors WHERE id = " + authorId, (row, number) -> {
+                Author a = new Author();
+                a.setId(row.getInt("id"));
+                a.setFirstName(row.getString("first_name"));
+                a.setLastName(row.getString("last_name"));
+                return a;
+            });
+            book.setAuthor(author.getFirstName() + " " + author.getLastName());
             return book;
         });
         return new ArrayList<>(books);
